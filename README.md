f1bf25673ef8a76966dddc7576f981db77dda508
root <root@localhost.localdomain>
2022-01-22 19:20:46 -0500

    Create main code

--- README.md
+++ README.md
@@ -3,7 +3,20 @@ Install script ocstore based on docker
 
 Consists of next containers: 
 - nginx
-- apache
+- apache php 7
 - mysql
 - backup
 - certbot
+
+
+How to use
+
+Install on CentOS 8
+
+clone repo or put files on servers
+
+run ./bootstrap.sh 
+
+default domain: https://example.ru/admin
+user: admin
+pass: admin
---
+++ docker/upload/admin/config.php
@@ -0,0 +1,37 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://example.ru/admin/');
+define('HTTP_CATALOG', 'https://example.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://example.ru/admin/');
+define('HTTPS_CATALOG', 'https://example.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/admin/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_STORAGE', '/var/www/storage/');
+define('DIR_CATALOG', '/var/www/html/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'db');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'pAssWord');
+define('DB_DATABASE', 'oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
